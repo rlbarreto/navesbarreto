3e8720b920558a4513603d0d1e29e332faa5015a
rafael <rafael@navesbarreto.com>
2013-10-21 19:50:20 -0200

    primeiro commit

--- server/main.js
+++ server/main.js
@@ -5,11 +5,14 @@ var express = require('express'),
     app = express();
 
 app.use(express.bodyParser());
-
+app.use(express.static('app'));
 app.get('/api/meusCarros', routes.meusCarros);
 
 app.use(function (req, res) {
     res.json({'ok': false, 'status': '404'});
 });
-
+app.listen(process.env.OPENSHIFT_NODEJS_PORT || 8080, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function() {
+    console.log('%s: Node server started on %s:%d ...',
+        Date(Date.now() ), process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+});
 module.exports = app;
